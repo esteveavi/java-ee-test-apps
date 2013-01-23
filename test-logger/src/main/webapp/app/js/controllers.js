147cd23c562956891c219882f938ad6dbd499ae4
esteveavi <eavilesa@gmail.com>
2013-01-23 13:22:09 +0100

    COntrollers

--- test-logger/src/main/webapp/app/js/controllers.js
+++ test-logger/src/main/webapp/app/js/controllers.js
@@ -30,31 +30,52 @@ function MyCtrl1($scope, $timeout, $log) {
 
 function FileUploadCntl($scope, $timeout, $log, $http) {
 	$scope.createUser = function() {
-		$http({
-		method: 'POST',
-		url: '../rest/customer/upload/process',
-		data: $scope.uploadedFile,
-		headers: {'Content-Type': 'multipart/form-data'}
+
+		var fileInput = document.getElementById('uploadedFile');
+		var file = fileInput.files[0];
+		var formData = new FormData();
+		formData.append('uploadedFile', file);
+
+		var fd = new FormData();
+		fd.append('uploadedFile', $scope.uploadedFile);
+
+		$http.post('../rest/customer/upload/process', formData, {
+			transformRequest: angular.identity,
+			headers: {
+				'Content-Type': undefined
+			}
 		});
+
 	}
+
 }
 
 
 
-function MyCtrl2($scope, $timeout, $log, MonitoringService) {
+function MyCtrl2($scope, $timeout, $log, MonitoringService, loadingService) {
 	var timeoutId;
 
+	$scope.$watch(function() { 
+		//alert('Count '+loadingService.requestCount);
+		return loadingService.isLoading(); 
+		
+		}
+	, 
+		function(value) { 
+			$scope.loading = value; });
+	
+	
 	$scope.title = 'Lorem Ipsum';
 	$scope.chartData = [];
 	$scope.monitoringData = new Object();
 
 	$scope.updateDate = function(){
 		var monitoringDataResponse = MonitoringService.get(function(){
-				$scope.monitoringData = monitoringDataResponse;
-				$scope.$broadcast('MonitoringDataUpdated');
-				$log.log("Data Updated");
-				
-			}
+			$scope.monitoringData = monitoringDataResponse;
+			$scope.$broadcast('MonitoringDataUpdated');
+			$log.log("Data Updated");
+
+		}
 		);
 	}
 
@@ -77,34 +98,84 @@ function RequestChartCntl($scope, $log ) {
 	$scope.dataPropWebappMetricsFilter = "com.yammer.metrics.web.WebappMetricsFilter";
 	$scope.$on('MonitoringDataUpdated', function() {
 		$log.log("RequestChartCntl ");
-	    var requestsMean = new Number($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.mean);
+		var requestsMean = new Number($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.mean);
 		requestsMean = requestsMean.toFixed(2);
 		$scope.chartData = [
 		                    ['Label', 'Value'],
 		                    ['Req/'+$scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.rate.unit, Math.round($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.rate.m1)],
 		                    ['Dur('+$scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.unit+')', requestsMean*1]
 		                    ];	
-		
-	    
-	  });
+
+
+	});
 }
+
+
+function ActiveRequestChartCntl($scope, $log ) {
+	$scope.chartData = [];
+	$scope.dataPropWebappMetricsFilter = "com.yammer.metrics.web.WebappMetricsFilter";
+	$scope.$on('MonitoringDataUpdated', function() {
+		var requestsMean = new Number($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.mean);
+		requestsMean = requestsMean.toFixed(2);
+		$scope.chartData = [
+		                    ['Label', 'Value'],
+		                    ['Active Req', Math.round($scope.monitoringData[$scope.dataPropWebappMetricsFilter].activeRequests.count)],
+		                    ];	
+	});
+}
+
+
+function RequestsCandleStickChartCntl($scope, $log, $filter ) {
+	$scope.dataPropWebappMetricsFilter = "com.yammer.metrics.web.WebappMetricsFilter";
+	
+	$scope.chartData = [
+	                    ['0', 0, 0, 0, 0],
+	                    ['0', 0, 0, 0, 0],
+	                    ['0', 0, 0, 0, 0],
+	                    ['0', 0, 0, 0, 0],
+	                    ['0', 0, 0, 0, 0],
+	                    ['0', 0, 0, 0, 0],
+	                    ['0', 0, 0, 0, 0],
+	                    ['0', 0, 0, 0, 0],
+	                    ['0', 0, 0, 0, 0],
+	                    ['0',0, 0, 0, 0]
+	                    ];
+	
+	$scope.$on('MonitoringDataUpdated', function() {
+		$scope.unit = $scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.unit;
+		var requestDurationMin = Math.round($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.min);
+		var requestDurationMax = Math.round($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.max);
+		var requestDurationMean = Math.round($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.mean);
+		var requestDurationStdDev = Math.round($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.std_dev);
+		var tempData = [];
+		angular.copy($scope.chartData, tempData);
+		var now = new Date();
+		var currentTime = $filter('date') (now, 'mediumTime') ;
+		tempData.splice(0, 1);
+		tempData.push([ currentTime , requestDurationMin, requestDurationMean-requestDurationStdDev,requestDurationMean+requestDurationStdDev, requestDurationMax]);
+		$scope.chartData = tempData;
+		$log.log("Chart Data: "+$scope.chartData);
+	});
+}
+
+
 
 
 function MemoryChartCntl($scope, $log ) {
 	$scope.chartData = [];
 	$scope.dataPropJVMMemory = "jvm.vm.memory";
 	$scope.$on('MonitoringDataUpdated', function() {
-		
-	    var requestsMean = new Number($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.mean);
+
+		var requestsMean = new Number($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.mean);
 		requestsMean = requestsMean.toFixed(2);
 		$scope.chartData = [
 		                    ['Label', 'Value'],
 		                    ['Req/'+$scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.rate.unit, Math.round($scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.rate.m1)],
 		                    ['Dur('+$scope.monitoringData[$scope.dataPropWebappMetricsFilter].requests.duration.unit+')', requestsMean*1]
 		                    ];	
-		
-	    
-	  });
+
+
+	});
 }
 
 
@@ -117,17 +188,17 @@ function ThreadCountCntl($scope, $log, $filter ) {
 	                    ['0', 0, 0],['0', 0, 0],['0', 0, 0],['0', 0, 0],['0', 0, 0],['0', 0, 0],['0', 0, 0],['0', 0, 0],['0', 0, 0],['0',0, 0]
 	                    ];
 	$scope.$on('MonitoringDataUpdated', function() {
-	    var threadCount = $scope.monitoringData[$scope.dataPropThreadCount].thread_count;
-	    var daemonThreadCount = $scope.monitoringData[$scope.dataPropThreadCount].daemon_thread_count;
-	    var tempData = [];
-	    angular.copy($scope.chartData, tempData);
-	    $log.log("ThreadCountCntl Updating to "+threadCount);
-	    var currentTime = $filter('date')($scope.monitoringData[$scope.dataPropThreadCount].current_time, 'mediumTime');
+		var threadCount = $scope.monitoringData[$scope.dataPropThreadCount].thread_count;
+		var daemonThreadCount = $scope.monitoringData[$scope.dataPropThreadCount].daemon_thread_count;
+		var tempData = [];
+		angular.copy($scope.chartData, tempData);
+		$log.log("ThreadCountCntl Updating to "+threadCount);
+		var currentTime = $filter('date')($scope.monitoringData[$scope.dataPropThreadCount].current_time, 'mediumTime');
 		tempData.splice(1, 1);
 		tempData.push([ currentTime , threadCount, daemonThreadCount]);
 		$scope.chartData = tempData;
-	    $log.log("Chart Data: "+$scope.chartData);
-	  });
+		$log.log("Chart Data: "+$scope.chartData);
+	});
 }
 
 function MemorytCntl($scope, $log, $filter ) {
@@ -140,18 +211,18 @@ function MemorytCntl($scope, $log, $filter ) {
 	                    ];
 	$scope.$on('MonitoringDataUpdated', function() {
 		var currentTime = $filter('date')($scope.monitoringData[$scope.dataPropJVM].current_time, 'mediumTime');
-	    var totalInit = $scope.monitoringData[$scope.dataPropJVM].memory.totalInit;
-	    var totalUsed = $scope.monitoringData[$scope.dataPropJVM].memory.totalUsed;
-	    var totalMax = $scope.monitoringData[$scope.dataPropJVM].memory.totalMax;
-	    var totalCommitted = $scope.monitoringData[$scope.dataPropJVM].memory.totalCommitted;
+		var totalInit = $scope.monitoringData[$scope.dataPropJVM].memory.totalInit;
+		var totalUsed = $scope.monitoringData[$scope.dataPropJVM].memory.totalUsed;
+		var totalMax = $scope.monitoringData[$scope.dataPropJVM].memory.totalMax;
+		var totalCommitted = $scope.monitoringData[$scope.dataPropJVM].memory.totalCommitted;
 
-	    var tempData = [];
-	    angular.copy($scope.chartData, tempData);
+		var tempData = [];
+		angular.copy($scope.chartData, tempData);
 		tempData.splice(1, 1);
 		tempData.push([ currentTime , totalInit, totalUsed, totalMax, totalCommitted]);
 		$scope.chartData = tempData;
-	    $log.log("Chart Data: "+$scope.chartData);
-	  });
+		$log.log("Chart Data: "+$scope.chartData);
+	});
 }
 
 function MemoryHeaptCntl($scope, $log, $filter ) {
@@ -164,18 +235,18 @@ function MemoryHeaptCntl($scope, $log, $filter ) {
 	                    ];
 	$scope.$on('MonitoringDataUpdated', function() {
 		var currentTime = $filter('date')($scope.monitoringData[$scope.dataPropJVM].current_time, 'mediumTime');
-	    var totalInit = $scope.monitoringData[$scope.dataPropJVM].memory.heapInit;
-	    var totalUsed = $scope.monitoringData[$scope.dataPropJVM].memory.heapUsed;
-	    var totalMax = $scope.monitoringData[$scope.dataPropJVM].memory.heapMax;
-	    var totalCommitted = $scope.monitoringData[$scope.dataPropJVM].memory.heapCommitted;
+		var totalInit = $scope.monitoringData[$scope.dataPropJVM].memory.heapInit;
+		var totalUsed = $scope.monitoringData[$scope.dataPropJVM].memory.heapUsed;
+		var totalMax = $scope.monitoringData[$scope.dataPropJVM].memory.heapMax;
+		var totalCommitted = $scope.monitoringData[$scope.dataPropJVM].memory.heapCommitted;
 
-	    var tempData = [];
-	    angular.copy($scope.chartData, tempData);
+		var tempData = [];
+		angular.copy($scope.chartData, tempData);
 		tempData.splice(1, 1);
 		tempData.push([ currentTime , totalInit, totalUsed, totalMax, totalCommitted]);
 		$scope.chartData = tempData;
-	    $log.log("Chart Data: "+$scope.chartData);
-	  });
+		$log.log("Chart Data: "+$scope.chartData);
+	});
 }
 
 google.load('visualization', '1',  {packages: ['gauge', "corechart"]});   // Don't need to specify chart libraries!
